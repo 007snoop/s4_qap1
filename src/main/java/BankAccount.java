0ff994e33f94dd1c7652b395ba8e5f665ee72d37
Colin Yetman <colinguitarist@gmail.com>
2025-05-26 15:07:23 -0230

    - test initial account balance pass - test deposit feedback pass {TODO} - Add github actions for running these tests automatically

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
-
 public class BankAccount {
+    private final String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountHolder, double initialBalance) {
+
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+
+        if (initialBalance < 0) {
+            throw new IllegalArgumentException("Initial balance cannot be below 0.");
+        }
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Deposit amount must be above 0.");
+        }
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Withdraw must be at least 0.01");
+        }
+        if (amount > balance) {
+            throw new IllegalArgumentException("Not enough funds.");
+        }
+        balance -= amount;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "accountHolder= '" + accountHolder + '\'' +
+                ", balance= $" + balance +
+                '}';
+    }
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankAccountTest {
+
+    public BankAccount account;
+
+    @BeforeEach
+    void setUp() {
+        account = new BankAccount("Colin", 400);
+    }
+
+    @Test
+    void testIniBal() {
+        assertEquals(400, account.getBalance());
+    }
+
+    @Test
+    void testDeposit() {
+        account.deposit(5.0);
+        assertEquals(405, account.getBalance());
+    }
 }
